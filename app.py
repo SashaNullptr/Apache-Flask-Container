f1c256546b3972bd21fb30337f4f1f98fd30b44e
Sasha Elaine Fox <sashanullptr@gmail.com>
2019-07-10 17:53:05 -0600

    Basic example Flask app.

---
+++ app.py
@@ -0,0 +1,39 @@
+# System
+import json
+
+# Third Party
+from flask import Flask, json
+from flask_cors import CORS
+
+# Local
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/example', methods=['GET'])
+def counter_endpoint( event=None, context=None ):
+    """
+    API endpoint that returns a generic greeting
+
+    An example request might look like:
+
+    .. sourcecode:: http
+
+       GET www.x.com/example HTTP/1.1
+       Host: example.com
+       Accept: application/json, text/javascript
+
+    Results will be returned as JSON object with the following format:
+
+    .. code-block:: json
+
+        {
+          "hello": "world"
+        }
+
+    """
+
+    return app.response_class( json.dumps( {"hello":"world"} ), mimetype='application/json' )
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
